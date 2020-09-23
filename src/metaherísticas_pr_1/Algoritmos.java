28fad784a12f5eb9c966db4508e6a9806d8646bb
Miguerubsk <miguerubsk@gmail.com>
2020-09-23 21:45:04 +0200

    Create Algoritmos.java

---
+++ src/metaherísticas_pr_1/Algoritmos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaherísticas_pr_1;
+
+/**
+ *
+ * @author Miguerubsk
+ */
+public class Algoritmos {
+    
+}
