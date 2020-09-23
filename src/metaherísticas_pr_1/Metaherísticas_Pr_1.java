7e199bfcd81dd20d094ca04f6b5f32ee92760b59
Miguerubsk <miguerubsk@gmail.com>
2020-09-23 11:13:02 +0200

    Initial commit

---
+++ src/metaherísticas_pr_1/Metaherísticas_Pr_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaherísticas_pr_1;
+
+/**
+ *
+ * @author Miguerubsk
+ */
+public class Metaherísticas_Pr_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
