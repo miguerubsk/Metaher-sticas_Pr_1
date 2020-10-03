11d576f380f1f66b2d91fd2d22247df4a6b9937f
Miguerubsk <miguerubsk@gmail.com>
2020-10-03 14:01:47 +0200

    Búsqueda Local
    Empezada la búsqueda local con algunas funciones auxiliares

--- src/metaherísticas_pr_1/Algoritmos.java
+++ src/metaherísticas_pr_1/Algoritmos.java
@@ -11,8 +11,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import tools.CargaDatos;
 import java.util.Random;
+import java.util.Vector;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
+import tools.Configurador;
 import tools.Timer;
 
 /**
@@ -23,16 +25,22 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
 
     private Random aleatorio;
     private CargaDatos archivo;
+    private Configurador config;
     private StringBuilder log;
     private CountDownLatch cdl;
     private HashSet<Integer> sol;
     private String algoritmo;
     private Timer Tiempo;
     private long Semilla;
+    Vector<Double> Aportes;
+    Vector<Boolean> Marcados ;
 
-    public Algoritmos(CargaDatos archivo, CountDownLatch cdl, Long semilla, String algoritmo) {
+    public Algoritmos(CargaDatos archivo, CountDownLatch cdl, Long semilla, String algoritmo, Configurador config) {
         this.archivo = archivo;
+        this.config = config;
         this.cdl = cdl;
+        this.Aportes = new Vector<>();
+        this.Marcados = new Vector<>();
         this.Semilla = semilla;
         aleatorio = new Random(semilla);
         log = new StringBuilder();
@@ -49,7 +57,7 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
                 double Coste = Greedy();
 //                Tiempo.Stop();
                 long Stop = System.currentTimeMillis();
-                System.out.println("Archivo: "+archivo.getNombreFichero()+ "\nSemilla: "+ Semilla +"\nmetaherísticas_pr_1.Algoritmos.run(): greedy" + sol.toString() + "\nTiempo: " + ((Stop-Start)) + " ms" + "\nCoste Solución: " + Coste +"\n\n");
+                System.out.println("Archivo: " + archivo.getNombreFichero() + "\nSemilla: " + Semilla + "\nmetaherísticas_pr_1.Algoritmos.run(): greedy" + sol.toString() + "\nTiempo: " + ((Stop - Start)) + " ms" + "\nCoste Solución: " + Coste + "\n\n");
                 break;
             case ("Búsqueda_Local"):
 //                BusquedaLocal();
@@ -69,10 +77,11 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
         return log.toString();
     }
 
-    void GenerarSolucionAleatoria() {
+    private double GenerarSolucionAleatoria() {
         while (sol.size() < archivo.getTamSolucion()) {
             sol.add(aleatorio.nextInt() * 500);
         }
+        return CosteSolución();
     }
 
     private double CostePunto(int punto) {
@@ -83,8 +92,8 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
         }
         return distancia;
     }
-    
-    private double CosteSolución(){
+
+    private double CosteSolución() {
         double distancia = 0.0;
         Iterator j = sol.iterator();
         j.next();
@@ -96,6 +105,28 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
         return distancia;
     }
 
+    private int PosAporteMenor() {
+        int pos = 0;
+        double menor = 999999999;
+        for(int i = 0; i < Aportes.size(); ++i){
+            if(!(Marcados.get(i)) && Aportes.get(i) < menor){
+                menor= Aportes.get(i);
+                pos = i;
+            }
+        }
+        Marcados.insertElementAt(true, pos);
+        return pos;
+    }
+
+    private boolean Esta(int ele){
+        Iterator<Integer> i = sol.iterator();
+        while(i.hasNext()){
+            if(i.next() == ele)
+                return true;
+        }
+        return false;
+    }
+    
     private double Greedy() {
         double mayordist = 0.0;
         ArrayList<Integer> M = new ArrayList();
@@ -127,9 +158,20 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
     }
 
     private double BusquedaLocal() {
-        GenerarSolucionAleatoria();
-        throw new UnsupportedOperationException("No soportado.");
-//        return CosteSolución();
+        boolean mejora = true;
+        int iteracion = 0, pos1, pos2;
+        double CosteActual = GenerarSolucionAleatoria();
+
+        while (iteracion < config.getEvaluaciones() && mejora) {
+            mejora = false;
+            Iterator<Integer> i = sol.iterator();
+            while (i.hasNext()) {
+                Aportes.add(CostePunto(i.next()));
+                Marcados.add(false);
+            }
+            
+        }
+        return CosteSolución();
     }
 
     private double BusquedaTabu() {
