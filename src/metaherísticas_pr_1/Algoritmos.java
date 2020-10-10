fb232bd06c9b47195ca91caf26e76ba8ebf0d17b
Alfonso José Arroyo Loaisa <alfonsoarroyoloaisa@gmail.com>
2020-10-10 11:21:49 +0200

    Cambios en las estructuras

--- src/metaherísticas_pr_1/Algoritmos.java
+++ src/metaherísticas_pr_1/Algoritmos.java
@@ -21,35 +21,35 @@ import tools.Timer;
  *
  * @author Miguerubsk
  */
-public class Algoritmos implements Callable<HashSet<Integer>> {
+public class Algoritmos implements Callable<Vector<Integer>> {
 
     private Random aleatorio;
     private CargaDatos archivo;
     private Configurador config;
     private StringBuilder log;
     private CountDownLatch cdl;
-    private HashSet<Integer> sol;
+    private Vector<Integer> sol;
     private String algoritmo;
-    private Timer Tiempo;
-    private long Semilla;
-    Vector<Double> Aportes;
-    Vector<Boolean> Marcados;
+    private Timer tiempo;
+    private long semilla;
+    Vector<Double> aportes;
+    Vector<Boolean> marcados;
 
     public Algoritmos(CargaDatos archivo, CountDownLatch cdl, Long semilla, String algoritmo, Configurador config) {
         this.archivo = archivo;
         this.config = config;
         this.cdl = cdl;
-        this.Aportes = new Vector<>();
-        this.Marcados = new Vector<>();
-        this.Semilla = semilla;
+        this.aportes = new Vector<>();
+        this.marcados = new Vector<>();
+        this.semilla = semilla;
         aleatorio = new Random(semilla);
         log = new StringBuilder();
-        sol = new HashSet(archivo.getTamSolucion());
+        sol = new Vector<Integer>(archivo.getTamSolucion());
         this.algoritmo = algoritmo;
     }
 
     @Override
-    public HashSet<Integer> call() throws Exception {
+    public Vector<Integer> call() throws Exception {
         double coste;
         long start, stop;
 //        System.out.println(archivo.getMatriz().toString());
@@ -61,9 +61,10 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
 //                Tiempo.Stop();
                 stop = System.currentTimeMillis();
                 System.out.println("Archivo: " + archivo.getNombreFichero() + "\nSemilla: "
-                        + Semilla + "\nmetaherísticas_pr_1.Algoritmos.run(): greedy" + sol.toString()
-                        + "\nTiempo: " + ((stop - start)) + " ms" + "\nCoste Solución: " + coste 
+                        + semilla + "\nmetaherísticas_pr_1.Algoritmos.run(): greedy" + sol.toString()
+                        + "\nTiempo: " + ((stop - start)) + " ms" + "\nCoste Solución: " + coste
                         + "\nDatos: " + archivo.getTamMatriz() + ";" + archivo.getTamSolucion() + "\n\n");
+                
                 break;
             case ("Búsqueda_Local"):
 //                Tiempo.Start();
@@ -72,12 +73,14 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
 //                Tiempo.Stop();
                 stop = System.currentTimeMillis();
                 System.out.println("Archivo: " + archivo.getNombreFichero() + "\nSemilla: "
-                        + Semilla + "\nmetaherísticas_pr_1.Algoritmos.run(): busqueda local" + sol.toString()
+                        + semilla + "\nmetaherísticas_pr_1.Algoritmos.run(): busqueda local" + sol.toString()
                         + "\nTiempo: " + ((stop - start)) + " ms" + "\nCoste Solución: " + coste + "\n\n");
+                
                 break;
             case ("Búsqueda_Tabú"):
 //                BusquedaTabu();
                 System.out.println("metaherísticas_pr_1.Algoritmos.run(): Búsqueda_Tabú" + sol.toString());
+                
                 break;
         }
         cdl.countDown();
@@ -85,150 +88,105 @@ public class Algoritmos implements Callable<HashSet<Integer>> {
         return sol;
     }
 
-    public String getLog() {
-        return log.toString();
-    }
-
-    private void GenerarSolucionAleatoria() {
-        while (sol.size() < archivo.getTamSolucion()) {
-            sol.add(aleatorio.nextInt(archivo.getTamMatriz()));
-        }
-    }
-
-    private double CostePunto(int punto) {
-        double distancia = 0.0;
-        Iterator i = sol.iterator();
-        while (i.hasNext()) {
-            distancia += archivo.getMatriz()[punto][(int) i.next()];
-        }
-        return distancia;
-    }
-
-    private boolean Esta(int ele) {
-        Iterator<Integer> i = sol.iterator();
-        while (i.hasNext()) {
-            if (i.next() == ele) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    //Algoritmos
     private double Greedy() {
         double mayordist = 0.0;
-        ArrayList<Integer> M = new ArrayList();
+        
         Boolean[] marcados = new Boolean[archivo.getTamMatriz()];
         Arrays.fill(marcados, Boolean.FALSE);
 
         Integer punto = aleatorio.nextInt(archivo.getTamMatriz() - 1);
         marcados[punto] = true;
+        
+        int contador = 1;
         sol.add(punto);
 
         for (int i = 1; i < archivo.getTamSolucion(); i++) {
             double d = 0.0;
             for (int j = 0; j < archivo.getTamMatriz(); j++) {
                 if (!marcados[j]) {
-                    d = CostePunto(j);
+                    d = costePuntoEnSolucion(j);
                     if (d > mayordist) {
                         mayordist = d;
                         punto = j;
                     }
                     d = 0.0;
                 }
-
             }
             marcados[punto] = true;
             sol.add(punto);
+            
+            contador++;
+            
             mayordist = 0.0;
         }
-        return CosteSolución();
+        
+        return costeSolucion();
     }
 
     private double BusquedaLocal() {
-        long iteracion = 0;
-        GenerarSolucionAleatoria();
-        double costeActual = CosteSolución();
-
-        Iterator<Integer> i = sol.iterator();
-        while (i.hasNext()) {
-            Aportes.add(CostePunto(i.next()));
-            Marcados.add(false);
-        }
-
-        while (iteracion != 50000) {
-
-            int posAporteMenor = PosAporteMenor();
-            int contador = 0;
-            for (Integer integer : sol) {
-                if (contador == posAporteMenor) {
-                    sol.remove(integer);
-                    break;
-                }
-
-                contador++;
-            }
-
-            int j = 0;
-            boolean mejora = false;
-            while (j < archivo.getTamMatriz() && !mejora) {
-                sol.add((int) archivo.getMatriz()[j][0]);
-                double nuevoCoste = CosteSolución();
-                if (nuevoCoste < costeActual) {
-                    costeActual = nuevoCoste;
-                    mejora = true;
-                    break;
-                }
-
-                sol.remove((int) archivo.getMatriz()[j][0]);
-                j++;
-            }
-            
-            iteracion++;
-            
-//            System.out.println(iteracion);
-            if(iteracion ==50000){
-                System.out.println(iteracion);
-            }
-            
-            
-        }
-
-        return CosteSolución();
-    }
-
-    private double CosteSolución() {
-        double distancia = 0.0;
-        Iterator j = sol.iterator();
-        j.next();
-        for (int i = 0; i < sol.size(); i++) {
-            while (j.hasNext()) {
-                Iterator<Integer> k = sol.iterator();
-                for (int c = 0; c < i; c++)
-                    k.next();
-                distancia += archivo.getMatriz()[k.next()][(int)j.next()];
-            }
-        }
-        return distancia;
-    }
-
-    private int PosAporteMenor() {
-        int pos = 0;
-        double menor = 999999999;
-        for (int i = 0; i < Aportes.size(); ++i) {
-            if (!(Marcados.get(i)) && Aportes.get(i) < menor) {
-                menor = Aportes.get(i);
-                pos = i;
-            }
-        }
-        Marcados.insertElementAt(true, pos);
-        return pos;
+        return 0;
     }
 
     private double BusquedaTabu() {
         //TODO
-        GenerarSolucionAleatoria();
+        generarSolucionAleatoria();
         throw new UnsupportedOperationException("No soportado.");
 //        return CosteSolución();
     }
 
+    //Funciones auxiliares
+    private void generarSolucionAleatoria() {
+        for (int i = 0; i < sol.size(); i++) {
+            sol.add(aleatorio.nextInt(archivo.getTamMatriz()));
+        }
+    }
+
+    private double costePuntoEnSolucion(int punto) {
+        double distancia = 0.0;
+        for (int i = 0; i < sol.size(); i++) {
+            distancia += archivo.getMatriz()[punto][sol.get(i)];
+        }
+
+        return distancia;
+    }
+
+    private boolean estaEnLaSolucion(int ele) {
+        for (Integer integer : sol) {
+            if (integer == ele) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private double costeSolucion() {
+        double distancia = 0.0;
+
+        for (int i = 0; i < sol.size(); i++) {
+            for (int j = i + 1; j < sol.size(); j++) {
+                distancia += archivo.getMatriz()[sol.get(i)][sol.get(j)];
+            }
+
+        }
+
+        return distancia;
+    }
+
+    private int posicionAporteMenor() {
+        int pos = 0;
+        double menor = 999999999;
+        for (int i = 0; i < aportes.size(); ++i) {
+            if (!(marcados.get(i)) && aportes.get(i) < menor) {
+                menor = aportes.get(i);
+                pos = i;
+            }
+        }
+        marcados.insertElementAt(true, pos);
+        return pos;
+    }
+
+    public String getLog() {
+        return log.toString();
+    }
 }
